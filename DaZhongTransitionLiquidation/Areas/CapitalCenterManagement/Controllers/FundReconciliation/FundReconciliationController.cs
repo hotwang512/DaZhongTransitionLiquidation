ab686f4157efbd933677f91a436e362ee517e363
peter <peter@PETER-PC>
2018-11-19 10:36:22 +0800

    银行流水对账

---
+++ DaZhongTransitionLiquidation/Areas/CapitalCenterManagement/Controllers/FundReconciliation/FundReconciliationController.cs
@@ -0,0 +1,131 @@
+﻿using DaZhongTransitionLiquidation.Areas.CapitalCenterManagement.Controllers.BankFlowTemplate;
+using DaZhongTransitionLiquidation.Areas.PaymentManagement.Models;
+using DaZhongTransitionLiquidation.Common.Pub;
+using DaZhongTransitionLiquidation.Infrastructure.Dao;
+using DaZhongTransitionLiquidation.Infrastructure.UserDefinedEntity;
+using SqlSugar;
+using SyntacticSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace DaZhongTransitionLiquidation.Areas.CapitalCenterManagement.Controllers.FundReconciliation
+{
+    public class FundReconciliationController : BaseController
+    {
+        public FundReconciliationController(DbService dbService, DbBusinessDataService dbBusinessDataService) : base(dbService, dbBusinessDataService)
+        {
+
+        }
+        // GET: CapitalCenterManagement/FundReconciliation
+        public ActionResult Index()
+        {
+            ViewBag.CurrentModulePermission = GetRoleModuleInfo(MasterVGUID.BankData);
+            return View();
+        }
+        public JsonResult GetFundReconciliationData(Business_FundReconciliation searchParams, GridParams para)
+        {
+            var jsonResult = new JsonResultModel<Business_FundReconciliation>();
+            DbBusinessDataService.Command(db =>
+            {
+                int pageCount = 0;
+                para.pagenum = para.pagenum + 1;
+                jsonResult.Rows = db.Queryable<Business_FundReconciliation>()
+                .OrderBy(i => i.BalanceDate, OrderByType.Desc).ToPageList(para.pagenum, para.pagesize, ref pageCount);
+                jsonResult.TotalRows = pageCount;
+            });
+            return Json(jsonResult, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult DeleteFundReconciliation(List<Guid> vguids)//Guid[] vguids
+        {
+            var resultModel = new ResultModel<string>() { IsSuccess = false, Status = "0" };
+
+            DbBusinessDataService.Command(db =>
+            {
+                var data = db.Queryable<Business_FundReconciliation>();
+                foreach (var item in vguids)
+                {
+                    int saveChanges = 1;
+                    var isAny = data.Any(x => x.VGUID == item);
+                    if (isAny)
+                    {
+                        saveChanges = db.Deleteable<Business_FundReconciliation>(x => x.VGUID == item).ExecuteCommand();
+                    }
+                    resultModel.IsSuccess = saveChanges == vguids.Count;
+                    resultModel.Status = resultModel.IsSuccess ? "1" : "0";
+                }
+            });
+            return Json(resultModel);
+        }
+        public JsonResult SaveFundReconciliation(Business_FundReconciliation sevenSection)
+        {
+            var resultModel = new ResultModel<string>() { IsSuccess = false, Status = "0" };
+            DbBusinessDataService.Command(db =>
+            {
+                var any = "";
+                var result = db.Ado.UseTran(() =>
+                {
+                    var datas = db.Queryable<Business_BankFlowTemplate>();
+                    var balanceDate = sevenSection.BalanceDate;//余额日期
+                    var initialBalanceData = db.Queryable<Business_FundReconciliation>().OrderBy("BalanceDate desc").First();//最新一条数据
+                    var bankBalance = sevenSection.BankBalance;//银行余额 
+                    var date = balanceDate.ToString().Split(" ")[0];
+                    var initialBalance = datas.First().Balance;//初始余额
+                    var balanceDateEnd = Convert.ToDateTime(date + " 23:59:59");
+                    decimal number = 0;
+                    if (initialBalanceData != null)
+                    {
+                        if (initialBalanceData.BalanceDate != null)
+                        {
+                            //对账,日期只能后延
+                            if (balanceDate < initialBalanceData.BalanceDate)
+                            {
+                                any = "2";
+                                return;
+                            }
+                        }
+                        if (initialBalanceData.ReconciliantStatus == "对账失败")
+                        {
+                            any = "3";
+                            return;
+                        }
+                        number = db.Ado.SqlQuery<decimal>(@"select (SUM(TurnIn)-SUM(TurnOut)) as number from Business_BankFlowTemplate where  
+                                 TransactionDate>'" + initialBalanceData.BalanceDate + "' and TransactionDate<='" + balanceDateEnd + "' ").FirstOrDefault();
+                        if (bankBalance == (initialBalanceData.BankBalance + number))
+                        {
+                            sevenSection.ReconciliantStatus = "对账成功";
+                        }
+                        else
+                        {
+                            sevenSection.ReconciliantStatus = "对账失败";
+                        }
+                    }
+                    else
+                    {
+                        number = db.Ado.SqlQuery<decimal>(@"select (SUM(TurnIn)-SUM(TurnOut)) as number from Business_BankFlowTemplate where  
+                                 TransactionDate<='" + balanceDateEnd + "' ").FirstOrDefault();
+                        if (bankBalance == (initialBalance + number))
+                        {
+                            sevenSection.ReconciliantStatus = "对账成功";
+                        }
+                        else
+                        {
+                            sevenSection.ReconciliantStatus = "对账失败";
+                        }
+                    } 
+                    sevenSection.VGUID = Guid.NewGuid();
+                    db.Insertable(sevenSection).ExecuteCommand();
+                });
+                resultModel.IsSuccess = result.IsSuccess;
+                resultModel.ResultInfo = result.ErrorMessage;
+                resultModel.Status = resultModel.IsSuccess ? "1" : "0";
+                if (any != "")
+                {
+                    resultModel.Status = any;
+                }
+            });
+            return Json(resultModel);
+        }
+    }
+}
---
+++ DaZhongTransitionLiquidation/Areas/CapitalCenterManagement/Views/_js/FundReconciliation.js
@@ -0,0 +1,241 @@
+﻿//所有元素选择器
+var selector = {
+    $grid: function () { return $("#jqxTable") },
+    $btnAdd: function () { return $("#btnAdd") },
+    $btnDelete: function () { return $("#btnDelete") },
+    $btnSearch: function () { return $("#btnSearch") },
+    $btnReset: function () { return $("#btnReset") },
+
+    $txtDatedTime: function () { return $("#txtDatedTime") },
+    $AddNewBankDataDialog: function () { return $("#AddNewBankDataDialog") },
+    $AddNewBankData_OKButton: function () { return $("#AddNewBankData_OKButton") },
+    $AddNewBankData_CancelBtn: function () { return $("#AddNewBankData_CancelBtn") },
+
+    $VoucherSubject: function () { return $("#VoucherSubject") },
+    $VoucherSummary: function () { return $("#VoucherSummary") },
+
+    $txtBankAccountName_Dialog: function () { return $("#txtBankAccountName_Dialog") },
+    $txtBankAccount_Dialog: function () { return $("#txtBankAccount_Dialog") },
+    $txtBank_Dialog: function () { return $("#txtBank_Dialog") },
+
+    $pushPeopleDropDownButton: function () { return $("#pushPeopleDropDownButton") },
+    $pushTree: function () { return $("#pushTree") },
+
+    $txtChannel_Dialog: function () { return $("#txtChannel_Dialog") },
+    $txtRemark_Dialog: function () { return $("#txtRemark_Dialog") },
+    $EditPermission: function () { return $("#EditPermission") }
+}; //selector end
+
+var isEdit = false;
+var vguid = "";
+var $page = function () {
+
+    this.init = function () {
+        addEvent();
+    }
+
+    //所有事件
+    function addEvent() {
+        $("#btnAdd").on("click", function () {
+            $("#BankBalance").val("");
+            $("#BalanceDate").val("");
+            $("#ReconciliantDate").val("");
+            $("#Reconcilianter").val("");
+            selector.$AddNewBankDataDialog().modal({ backdrop: "static", keyboard: false });
+            selector.$AddNewBankDataDialog().modal("show");
+        });
+        $("#btnDelete").on("click", function () {
+            var selection = [];
+            var grid = $("#jqxTable");
+            var checedBoxs = grid.find(".jqx_datatable_checkbox:checked");
+            checedBoxs.each(function () {
+                var th = $(this);
+                if (th.is(":checked")) {
+                    var index = th.attr("index");
+                    var data = grid.jqxDataTable('getRows')[index];
+                    selection.push(data.VGUID);
+                }
+            });
+            if (selection.length < 1) {
+                jqxNotification("请选择您要删除的数据！", null, "error");
+            } else {
+                WindowConfirmDialog(dele, "您确定要删除选中的数据？", "确认框", "确定", "取消", selection);
+            }
+        });
+        //加载列表数据
+        initTable();
+        selector.$btnSearch().unbind("click").on("click", function () {
+            initTable();
+        });
+
+        //重置按钮事件
+        selector.$btnReset().on("click", function () {
+            //$("#TradingBank").val("");
+            //$("#TransactionDate").val("");
+            //$("#TransactionDateEnd").val("");
+            //$("#PaymentUnit").val("");
+        });
+
+        //弹出框中的取消按钮
+        selector.$AddNewBankData_CancelBtn().on("click", function () {
+            selector.$AddNewBankDataDialog().modal("hide");
+        });
+        //弹出框中的保存按钮
+        selector.$AddNewBankData_OKButton().on("click", function () {
+            var validateError = 0;//未通过验证的数量
+            if (!Validate($("#BankBalance"))) {
+                validateError++;
+            }
+            if (!Validate($("#Reconcilianter"))) {
+                validateError++;
+            }
+            if (validateError <= 0) {
+                $.ajax({
+                    url: "/CapitalCenterManagement/FundReconciliation/SaveFundReconciliation",
+                    data: {
+                        "BankBalance": $("#BankBalance").val(),
+                        "BalanceDate": $("#BalanceDate").val(),
+                        "ReconciliantDate": $("#ReconciliantDate").val(),
+                        "Reconcilianter": $("#Reconcilianter").val(),
+                        "VGUID": vguid
+                    },
+                    type: "post",
+                    dataType: "json",
+                    success: function (msg) {
+                        switch (msg.Status) {
+                            case "0":
+                                jqxNotification("保存失败！", null, "error");
+                                break;
+                            case "1":
+                                jqxNotification("保存成功！", null, "success");
+                                selector.$grid().jqxDataTable('updateBoundData');
+                                selector.$AddNewBankDataDialog().modal("hide");
+                                break;
+                            case "2":
+                                jqxNotification("余额日期有误！", null, "error");
+                                break;
+                            case "3":
+                                jqxNotification("前一天对账失败！", null, "error");
+                                break;
+                        }
+                    }
+                });
+            }
+        });
+
+    }; //addEvent end
+
+    //删除
+    function dele(selection) {
+        $.ajax({
+            url: "/CapitalCenterManagement/FundReconciliation/DeleteFundReconciliation",
+            //data: { vguids: selection },
+            data: { vguids: selection },
+            traditional: true,
+            type: "post",
+            success: function (msg) {
+                switch (msg.Status) {
+                    case "0":
+                        jqxNotification("删除失败！", null, "error");
+                        break;
+                    case "1":
+                        jqxNotification("删除成功！", null, "success");
+                        $("#jqxTable").jqxDataTable('updateBoundData');
+                        break;
+                }
+            }
+        });
+    }
+    function initTable() {
+        var source =
+            {
+                datafields:
+                [
+                    { name: "checkbox", type: null },
+                    { name: 'Reconcilianter', type: 'string' },
+                    { name: 'BankBalance', type: 'number' },
+                    { name: 'BalanceDate', type: 'date' },                    
+                    { name: 'ReconciliantDate', type: 'date' },
+                    { name: 'ReconciliantStatus', type: 'string' },
+                    { name: 'VGUID', type: 'string' },
+                ],
+                datatype: "json",
+                id: "VGUID",
+                data: { },
+                url: "/CapitalCenterManagement/FundReconciliation/GetFundReconciliationData"   //获取数据源的路径
+            };
+        var typeAdapter = new $.jqx.dataAdapter(source, {
+            downloadComplete: function (data) {
+                source.totalrecords = data.TotalRows;
+            }
+        });
+        //创建卡信息列表（主表）
+        selector.$grid().jqxDataTable(
+            {
+                pageable: true,
+                width: "100%",
+                height: 400,
+                pageSize: 10,
+                serverProcessing: true,
+                pagerButtonsCount: 10,
+                source: typeAdapter,
+                theme: "office",
+                columnsHeight: 30,
+                columns: [
+                    { text: "", datafield: "checkbox", width: 35, align: 'center', cellsAlign: 'center', cellsRenderer: cellsRendererFunc, renderer: rendererFunc, rendered: renderedFunc, autoRowHeight: false },
+                    { text: '银行余额', datafield: 'BankBalance', width: 300, cellsFormat: "d2", pinned: false, align: 'center', cellsAlign: 'center' },
+                    { text: '余额日期', datafield: 'BalanceDate', width: 300, pinned: false, align: 'center', cellsAlign: 'center', datatype: 'date', cellsformat: "yyyy-MM-dd" },
+                    { text: '对账日期', datafield: 'ReconciliantDate', width: 300, pinned: false, align: 'center', cellsAlign: 'center', datatype: 'date', cellsformat: "yyyy-MM-dd" },
+                    { text: '对账人', datafield: 'Reconcilianter', width: 300, align: 'center', cellsAlign: 'center' },
+                    { text: '对账状态', datafield: 'ReconciliantStatus',  align: 'center', cellsAlign: 'center' },
+                    { text: 'VGUID', datafield: 'VGUID', hidden: true },
+                ]
+            });
+
+    }
+
+    function detailFunc(row, column, value, rowData) {
+        var container = "";
+        if (selector.$EditPermission().val() == "1") {
+            container = "<a href='#' onclick=edit('" + rowData.VGUID + "','" + rowData.VoucherSubject + "','" + rowData.VoucherSummary + "','" + rowData.VoucherSubjectName + "') style=\"text-decoration: underline;color: #333;\">" + rowData.Batch + "</a>";
+        } else {
+            container = "<span>" + rowData.Batch + "</span>";
+        }
+        return container;
+    }
+
+    function cellsRendererFunc(row, column, value, rowData) {
+        return "<input class=\"jqx_datatable_checkbox\" index=\"" + row + "\" type=\"checkbox\"  style=\"margin:auto;width: 17px;height: 17px;\" />";
+    }
+
+    function rendererFunc() {
+        var checkBox = "<div id='jqx_datatable_checkbox_all' class='jqx_datatable_checkbox_all' style='z-index: 999; margin-left:7px ;margin-top: 7px;'>";
+        checkBox += "</div>";
+        return checkBox;
+    }
+
+    function renderedFunc(element) {
+        var grid = selector.$grid();
+        element.jqxCheckBox();
+        element.on('change', function (event) {
+            var checked = element.jqxCheckBox('checked');
+
+            if (checked) {
+                var rows = grid.jqxDataTable('getRows');
+                for (var i = 0; i < rows.length; i++) {
+                    grid.jqxDataTable('selectRow', i);
+                    grid.find(".jqx_datatable_checkbox").attr("checked", "checked")
+                }
+            } else {
+                grid.jqxDataTable('clearSelection');
+                grid.find(".jqx_datatable_checkbox").removeAttr("checked", "checked")
+            }
+        });
+        return true;
+    }
+};
+
+$(function () {
+    var page = new $page();
+    page.init();
+});

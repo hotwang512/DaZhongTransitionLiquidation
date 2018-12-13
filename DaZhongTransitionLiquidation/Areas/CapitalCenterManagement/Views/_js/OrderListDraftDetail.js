e5d722fad9c8715048593040c648367803e873ef
peter <peter@PETER-PC>
2018-12-13 18:00:03 +0800

    人民币转大写

--- DaZhongTransitionLiquidation/Areas/CapitalCenterManagement/Views/_js/OrderListDraftDetail.js
+++ DaZhongTransitionLiquidation/Areas/CapitalCenterManagement/Views/_js/OrderListDraftDetail.js
@@ -25,7 +25,7 @@ var $page = function () {
         $("#Money").blur(function () {
             var money = $("#Money").val();
             if (money != "") {
-                var value = Arabia_To_SimplifiedChinese(money);
+                var value = smalltoBIG(money);
                 $("#CapitalizationMoney").val(value);
                 $("#CapitalizationMoney").attr("title", $("#CapitalizationMoney").val())
             }
@@ -130,6 +130,7 @@ var $page = function () {
                 $("#DepartmentHead").val(msg.DepartmentHead);
                 $("#Cashier").val(msg.Cashier);
                 $("#Payee").val(msg.Payee);
+                $("#CapitalizationMoney").attr("title", $("#CapitalizationMoney").val())
             }
         });
     }
@@ -142,67 +143,29 @@ $(function () {
 });
 
 
-//阿拉伯数字转换为简写汉字
-function Arabia_To_SimplifiedChinese(Num) {
-    for (i = Num.length - 1; i >= 0; i--) {
-        Num = Num.replace(",", "")//替换Num中的“,”
-        Num = Num.replace(" ", "")//替换Num中的空格
+/** 数字金额大写转换(可以处理整数,小数,负数) */
+function smalltoBIG(n) {
+    var fraction = ['角', '分'];
+    var digit = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'];
+    var unit = [['元', '万', '亿'], ['', '拾', '佰', '仟']];
+    var head = n < 0 ? '欠' : '';
+    n = Math.abs(n);
+
+    var s = '';
+
+    for (var i = 0; i < fraction.length; i++) {
+        s += (digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, '');
     }
-    if (isNaN(Num)) { //验证输入的字符是否为数字
-        //alert("请检查小写金额是否正确");
-        return;
-    }
-    //字符处理完毕后开始转换，采用前后两部分分别转换
-    part = String(Num).split(".");
-    newchar = "";
-    //小数点前进行转化
-    for (i = part[0].length - 1; i >= 0; i--) {
-        if (part[0].length > 10) {
-            //alert("位数过大，无法计算");
-            return "";
-        }//若数量超过拾亿单位，提示
-        tmpnewchar = ""
-        perchar = part[0].charAt(i);
-        switch (perchar) {
-            case "0": tmpnewchar = "零" + tmpnewchar; break;
-            case "1": tmpnewchar = "壹" + tmpnewchar; break;
-            case "2": tmpnewchar = "贰" + tmpnewchar; break;
-            case "3": tmpnewchar = "叁" + tmpnewchar; break;
-            case "4": tmpnewchar = "肆" + tmpnewchar; break;
-            case "5": tmpnewchar = "伍" + tmpnewchar; break;
-            case "6": tmpnewchar = "陆" + tmpnewchar; break;
-            case "7": tmpnewchar = "柒" + tmpnewchar; break;
-            case "8": tmpnewchar = "捌" + tmpnewchar; break;
-            case "9": tmpnewchar = "玖" + tmpnewchar; break;
+    s = s || '整';
+    n = Math.floor(n);
+
+    for (var i = 0; i < unit[0].length && n > 0; i++) {
+        var p = '';
+        for (var j = 0; j < unit[1].length && n > 0; j++) {
+            p = digit[n % 10] + unit[1][j] + p;
+            n = Math.floor(n / 10);
         }
-        switch (part[0].length - i - 1) {
-            case 0: tmpnewchar = tmpnewchar; break;
-            case 1: if (perchar != 0) tmpnewchar = tmpnewchar + "拾"; break;
-            case 2: if (perchar != 0) tmpnewchar = tmpnewchar + "佰"; break;
-            case 3: if (perchar != 0) tmpnewchar = tmpnewchar + "仟"; break;
-            case 4: tmpnewchar = tmpnewchar + "万"; break;
-            case 5: if (perchar != 0) tmpnewchar = tmpnewchar + "拾"; break;
-            case 6: if (perchar != 0) tmpnewchar = tmpnewchar + "佰"; break;
-            case 7: if (perchar != 0) tmpnewchar = tmpnewchar + "仟"; break;
-            case 8: tmpnewchar = tmpnewchar + "亿"; break;
-            case 9: tmpnewchar = tmpnewchar + "拾"; break;
-        }
-        newchar = tmpnewchar + newchar;
+        s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + s;
     }
-    //替换所有无用汉字，直到没有此类无用的数字为止
-    while (newchar.search("零零") != -1 || newchar.search("零亿") != -1 || newchar.search("亿万") != -1 || newchar.search("零万") != -1) {
-        newchar = newchar.replace("零亿", "亿");
-        newchar = newchar.replace("亿万", "亿");
-        newchar = newchar.replace("零万", "万");
-        newchar = newchar.replace("零零", "零");
-    }
-    //替换以“一十”开头的，为“十”
-    if (newchar.indexOf("壹拾") == 0) {
-        newchar = newchar.substr(1);
-    }
-    //替换以“零”结尾的，为“”
-    if (newchar.lastIndexOf("零") == newchar.length - 1) {
-        newchar = newchar.substr(0, newchar.length - 1);
-    }
-    return newchar;
+    return head + s.replace(/(零.)*零元/, '元').replace(/(零.)+/g, '零').replace(/^整$/, '零元整');
 }

6e54ae9d374956df3ab27c15e54c006d494240b1
peter <peter@PETER-PC>
2018-12-24 17:51:44 +0800

    订单打印页面

---
+++ DaZhongTransitionLiquidation/Areas/CapitalCenterManagement/Controllers/OrderListDraftPrint/OrderListDraftPrintController.cs
@@ -0,0 +1,23 @@
+﻿using DaZhongTransitionLiquidation.Common.Pub;
+using DaZhongTransitionLiquidation.Infrastructure.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DaZhongTransitionLiquidation.Areas.CapitalCenterManagement.Controllers.OrderListDraftPrint
+{
+    public class OrderListDraftPrintController : BaseController
+    {
+        public OrderListDraftPrintController(DbService dbService, DbBusinessDataService dbBusinessDataService) : base(dbService, dbBusinessDataService)
+        {
+        }
+        // GET: CapitalCenterManagement/OrderListDraftPrint
+        public ActionResult Index()
+        {
+            ViewBag.CurrentModulePermission = GetRoleModuleInfo(MasterVGUID.BankData);
+            return View();
+        }
+    }
+}

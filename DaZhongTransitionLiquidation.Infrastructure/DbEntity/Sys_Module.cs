a0413fb9c455148206ed6a8e5e721070d2ffd03d
hotwang512 <hot.wang@landasoft.com>
2019-01-18 15:29:01 +0800

    URL 自动生成版本号

--- DaZhongTransitionLiquidation.Infrastructure/DbEntity/Sys_Module.cs
+++ DaZhongTransitionLiquidation.Infrastructure/DbEntity/Sys_Module.cs
@@ -9,57 +9,60 @@ namespace DaZhongTransitionLiquidation.Infrastructure.DbEntity
     ///</summary>
     public class Sys_Module
     {
-           public Sys_Module(){
+        public Sys_Module()
+        {
 
-           }
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:False
-           /// </summary>           
-           public string ModuleName {get;set;}
+        }
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:False
+        /// </summary>           
+        public string ModuleName { get; set; }
 
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:True
-           /// </summary>           
-           public Guid? Parent {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:True
+        /// </summary>           
+        public Guid? Parent { get; set; }
 
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:True
-           /// </summary>           
-           public DateTime? CreatedDate {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:True
+        /// </summary>           
+        public DateTime? CreatedDate { get; set; }
 
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:True
-           /// </summary>           
-           public string CreatedUser {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:True
+        /// </summary>           
+        public string CreatedUser { get; set; }
 
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:True
-           /// </summary>           
-           public DateTime? ChangeDate {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:True
+        /// </summary>           
+        public DateTime? ChangeDate { get; set; }
 
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:True
-           /// </summary>           
-           public string ChangeUser {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:True
+        /// </summary>           
+        public string ChangeUser { get; set; }
 
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:False
-           /// </summary>           
-           public Guid Vguid {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:False
+        /// </summary>           
+        public Guid Vguid { get; set; }
+
+        public string Zorder { get; set; }
 
     }
 }
---
+++ DaZhongTransitionLiquidation/App_Start/MyUrlHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http.Routing;
+
+namespace System.Web.Mvc
+{
+    public static class MyUrlHelper
+    {
+        public static string CusContent(this UrlHelper helper, string value)
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            return helper.Content(string.Format(value + "?_v={0}", version));
+        }
+    }
+}

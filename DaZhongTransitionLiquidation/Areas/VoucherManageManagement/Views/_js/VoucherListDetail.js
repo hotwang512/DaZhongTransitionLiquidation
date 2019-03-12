ea65a8b59dffb5e2e5985c7b5a3821ada2d80fbf
peter <peter@PETER-PC>
2019-03-12 15:31:29 +0800

    凭证样式调整

--- DaZhongTransitionLiquidation/Areas/VoucherManageManagement/Views/_js/VoucherListDetail.js
+++ DaZhongTransitionLiquidation/Areas/VoucherManageManagement/Views/_js/VoucherListDetail.js
@@ -370,10 +370,10 @@ var $page = function () {
             moneyId = "LoanMoney_B_" + selectIndex;
         }
         var html = '<div id="' + id + '" class="' + className + '" style="border:2px solid #999;position:relative;border-radius:5px;margin: 15px 0;">' +
-           '<table id="" style="width:100%;">' +
+           '<table id="" style="width:100%;margin-left: 20px;">' +
                '<tr style="height:30px;">' +
                    '<td colspan="8" style="text-align: right;">' +
-                       '<div id="' + removeId + '" class="iconfont btn_icon remove" onclick="removes(' + removeId + ')" style="color: red !important;font-size: 20px !important;cursor:pointer;margin-left: 1480px;">&#xe6f2;</div>' +
+                       '<div id="' + removeId + '" class="iconfont btn_icon remove" onclick="removes(' + removeId + ')" style="color: red !important;font-size: 20px !important;cursor:pointer;margin-left: 1460px;">&#xe6f2;</div>' +
                    '</td>' +
                '</tr>' +
                '<tr style="height:55px">' +
@@ -523,10 +523,10 @@ var $page = function () {
                 moneyValue = data.LoanMoney == null ? "" : data.LoanMoney;
             }
             var html = '<div id="' + id + '" class="' + className + '" style="border:2px solid #999;position:relative;border-radius:5px;margin: 15px 0; width: 1510px;margin-left: 12px !important;">' +
-               '<table id="" style="width:100%;">' +
+               '<table id="" style="width:100%;margin-left: 20px;">' +
                    '<tr style="height:30px;">' +
                        '<td colspan="8" style="text-align: right;">' +
-                           '<div id="' + removeId + '" class="iconfont btn_icon remove" onclick="removes(' + removeId + ')" style="color: red !important;font-size: 20px !important;cursor:pointer;margin-left: 1480px;">&#xe6f2;</div>' +
+                           '<div id="' + removeId + '" class="iconfont btn_icon remove" onclick="removes(' + removeId + ')" style="color: red !important;font-size: 20px !important;cursor:pointer;margin-left: 1460px;">&#xe6f2;</div>' +
                        '</td>' +
                    '</tr>' +
                    '<tr style="height:55px">' +

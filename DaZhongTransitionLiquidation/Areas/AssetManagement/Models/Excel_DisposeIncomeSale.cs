f6ed98d043b4bfe6726c32a9592b4cf165ceb67f
landa <landa@DESKTOP-90C238M>
2019-09-24 15:10:21 +0800

    ，导入拍卖、出售、报废修改

--- DaZhongTransitionLiquidation/Areas/AssetManagement/Models/Excel_DisposeIncomeSale.cs
+++ DaZhongTransitionLiquidation/Areas/AssetManagement/Models/Excel_DisposeIncomeSale.cs
@@ -8,15 +8,16 @@ namespace DaZhongTransitionLiquidation.Areas.AssetManagement.Models
     public class Excel_DisposeIncomeSale
     {
         public string ImportPlateNumber { get; set; }
-        public string VehicleModel { get; set; }
-        public string CommissioningDate { get; set; }
-        public string TransactionPrice { get; set; }
-        public string ProcedureFee { get; set; }
-        public string SettlementPrice { get; set; }
-        public string UseDepartment { get; set; }
-        public string VehicleOwner { get; set; }
-        public string BackCarDate { get; set; }
-        public string Remark { get; set; }
-        public string BusinessModel { get; set; }
+        public string DisposeIncomeValue { get; set; }
+        //public string VehicleModel { get; set; }
+        //public string CommissioningDate { get; set; }
+        //public string TransactionPrice { get; set; }
+        //public string ProcedureFee { get; set; }
+        //public string SettlementPrice { get; set; }
+        //public string UseDepartment { get; set; }
+        //public string VehicleOwner { get; set; }
+        //public string BackCarDate { get; set; }
+        //public string Remark { get; set; }
+        //public string BusinessModel { get; set; }
     }
 }

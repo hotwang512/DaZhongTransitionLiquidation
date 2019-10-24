21944dada10cf5ba5ff6574798a85862745b98c6
landa <landa@DESKTOP-90C238M>
2019-10-24 12:12:59 +0800

    车辆核定表显示修改

--- DaZhongTransitionLiquidation/Areas/AssetManagement/Views/_js/AssetReport.js
+++ DaZhongTransitionLiquidation/Areas/AssetManagement/Views/_js/AssetReport.js
@@ -9,7 +9,6 @@ var selector = {
 }; //selector end
 var isEdit = false;
 var vguid = "";
-var values = new Array();
 var $page = function () {
     this.init = function () {
         var date = new Date;
@@ -18,44 +17,12 @@ var $page = function () {
         month = (month < 10 ? "0" + month : month);
         var currentDate = (year.toString() + "-" + month.toString());
         $("#YearMonth").val(currentDate);
-        //获取车型
-        $.ajax({
-            data: { YearMonth: $("#YearMonth").val() },
-            url: "/AssetManagement/AssetReport/GetAssetReportVehicleModel",
-            //traditional: true,
-            type: "post",
-            success: function (msg) {
-                debugger;
-                for (var i = 0; i < msg.length; i++) {
-                    var valueObj = new Object();
-                    valueObj.dataField = 'VehicleModel' + i;
-                    valueObj['function'] = 'sum';
-                    valueObj.text = msg[i];
-                    valueObj.width = 80;
-                    valueObj.align = 'center';
-                    valueObj.sortable = false;
-                    valueObj.formatSettings = { prefix: '', decimalPlaces: 2 };
-                    values.push(valueObj);
-                }
-                var valueObj1 = new Object();
-                valueObj1.dataField = 'Quantity';
-                valueObj1['function'] = 'sum';
-                valueObj1.text = 'total';
-                valueObj1.width = 80;
-                valueObj1.align = 'center';
-                valueObj1.sortable = false;
-                valueObj1.formatSettings = { prefix: '', decimalPlaces: 2 };
-                values.push(valueObj1);
-                addEvent();
-            }
-        });
-        
+        addEvent();
     }
     //所有事件
     function addEvent() {
         //加载列表数据
         initPeriodTable();
-        initPeriodTable1();
         //提交
         $("#btnSubmit").on("click", function () {
             $.ajax({
@@ -81,7 +48,6 @@ var $page = function () {
         });
         $("#btnGetData").on("click", function () {
             initPeriodTable();
-            //initEndPeriodTable();
         });
         //关闭
         $("#AssetReviewDialog_CancelBtn").on("click",
@@ -92,49 +58,62 @@ var $page = function () {
     }; //addEvent end
     function initPeriodTable() {
         layer.load();
-        getPeriodData(function (data) {
-            var datashow = fillData(data);
-            var datafields = new Array();
-            datafields.push({ name: 'Company', type: 'string' });
-            datafields.push({ name: 'PeriodType', type: 'string' });
-            datafields.push({ name: 'Quantity', type: 'number' });
-            for (var i = 0; i < values.length; i++) {
-                datafields.push({ name: 'VehicleModel' + i, type: 'number' });
-            }
-            debugger;
-            var source =
-            {
-                localdata: datashow,
-                datafields:datafields,
-                datatype: "json"
-            };
-            var dataAdapter = new $.jqx.dataAdapter(source);
-            dataAdapter.dataBind();
-            var pivotDataSource = new $.jqx.pivot(
-                dataAdapter,
+        getPeriodData(function (msg) {
+            switch (msg.Status) {
+            case "0":
+                jqxNotification("获取数据失败！", null, "error");
+                break;
+            case "1":
+                var values = fillValues(msg.ResultInfo);
+                var datashow = fillData(msg.ResultInfo2);
+                var datafields = new Array();
+                datafields.push({ name: 'CompanyType', type: 'string' });
+                datafields.push({ name: 'Company', type: 'string' });
+                datafields.push({ name: 'PeriodType', type: 'string' });
+                datafields.push({ name: 'Quantity', type: 'number' });
+                for (var i = 0; i < values.length; i++) {
+                    datafields.push({ name: 'VehicleModel' + i, type: 'number' });
+                }
+                var source =
                 {
-                    pivotValuesOnRows: false,
-                    totals: { rows: { subtotals: false, grandtotals: true }, columns: { subtotals: false, grandtotals: false } },
-                    rows: [{ dataField: 'Company', text: '管理公司', align: 'center', width: 150, isHidden: true}],
-                    columns: [{ dataField: 'PeriodType', width: 110, align: 'center' }],
-                    values: values
-                });
+                    localdata: datashow,
+                    datafields: datafields,
+                    datatype: "json"
+                };
+                var dataAdapter = new $.jqx.dataAdapter(source);
+                dataAdapter.dataBind();
+                var pivotDataSource = new $.jqx.pivot(
+                    dataAdapter,
+                    {
+                        pivotValuesOnRows: false,
+                        totals: { rows: { subtotals: false, grandtotals: false }, columns: { subtotals: false, grandtotals: false } },
+                        rows: [{ dataField: 'CompanyType' }, { dataField: 'Company' }],
+                        columns: [{ dataField: 'PeriodType', width: 110, align: 'center' }],
+                        values: values
+                    });
                 selector.$grid().jqxPivotGrid(
-                {
-                    source: pivotDataSource,
-                    treeStyleRows: false,
-                    autoResize: true,
-                    selectionEnabled: false
-                });
+                    {
+                        source: pivotDataSource,
+                        treeStyleRows: true,
+                        autoResize: true,
+                        selectionEnabled: true
+                    });
+                var pivotGrid = selector.$grid().jqxPivotGrid('getInstance');
+                var pivotRows = pivotGrid.getPivotRows();
+                for (var k = 0; k < pivotRows.items.length; k++) {
+                    pivotRows.items[k].expand();
+                }
+                pivotGrid.refresh();
+                break;
+            }
             layer.closeAll('loading');
         });
     }
     function fillData(data) {
         var newData = [];
         for (var j = 0; j < data.length; j++) {
-            var currentData = { "Company": data[j].Company, "PeriodType": data[j].PeriodType }//PeriodType
+            var currentData = {  "CompanyType": data[j].CompanyType, "PeriodType": data[j].PeriodType ,"Company": data[j].Company}//PeriodType
             var sum = 0;
-            debugger;
             for (var k = 0; k < data[j].ResultVehicleModelList.length; k++) {
                 currentData["VehicleModel" + k] = data[j].ResultVehicleModelList[k].Quantity;
                 sum += data[j].ResultVehicleModelList[k].Quantity;
@@ -145,63 +124,35 @@ var $page = function () {
         debugger;
         return newData;
     }
-    function initPeriodTable1() {
-        layer.load();
-        getPeriodData1(function (data) {
-            var datashow = fillData(data);
-            var datafields = new Array();
-            datafields.push({ name: 'Company', type: 'string' });
-            datafields.push({ name: 'PeriodType', type: 'string' });
-            datafields.push({ name: 'Quantity', type: 'number' });
-            for (var i = 0; i < values.length; i++) {
-                datafields.push({ name: 'VehicleModel' + i, type: 'number' });
-            }
-            debugger;
-            var source =
-            {
-                localdata: datashow,
-                datafields: datafields,
-                datatype: "json"
-            };
-            var dataAdapter = new $.jqx.dataAdapter(source);
-            dataAdapter.dataBind();
-            var pivotDataSource = new $.jqx.pivot(
-                dataAdapter,
-                {
-                    pivotValuesOnRows: false,
-                    totals: { rows: { subtotals: false, grandtotals: true }, columns: { subtotals: false, grandtotals: false } },
-                    rows: [{ dataField: 'Company', text: '所属公司', align: 'center', width: 150, isHidden: true }],
-                    columns: [{ dataField: 'PeriodType', width: 110, align: 'center' }],
-                    values: values
-                });
-            selector.$grid1().jqxPivotGrid(
-                {
-                    source: pivotDataSource,
-                    treeStyleRows: false,
-                    autoResize: true,
-                    selectionEnabled: false
-                });
-            layer.closeAll('loading');
-        });
+    function fillValues(data) {
+        var values = [];
+        for (var i = 0; i < data.length; i++) {
+            var valueObj = new Object();
+            valueObj.dataField = 'VehicleModel' + i;
+            valueObj.function = 'sum';
+            valueObj.text = data[i];
+            valueObj.width = 80;
+            valueObj.align = 'center';
+            valueObj.sortable = false;
+            valueObj.formatSettings = { prefix: '', decimalPlaces: 0 };
+            values.push(valueObj);
+        }
+        var QuantityObj = new Object();
+        QuantityObj.dataField = 'Quantity';
+        QuantityObj.function = 'sum';
+        QuantityObj.text = '小计';
+        QuantityObj.width = 80;
+        QuantityObj.align = 'center';
+        QuantityObj.sortable = false;
+        QuantityObj.formatSettings = { prefix: '', decimalPlaces: 0 };
+        values.push(QuantityObj);
+        return values;
     }
-   
     var tableValue = "";
     function getPeriodData(callback) {
         $.ajax({
             url: "/AssetManagement/AssetReport/GetManageCompanyAssetReport",
             data: { "YearMonth": $("#YearMonth").val()},
-            datatype: "json",
-            type: "post",
-            success: function (result) {
-                tableValue = result;
-                callback(result);
-            }
-        });
-    }
-    function getPeriodData1(callback) {
-        $.ajax({
-            url: "/AssetManagement/AssetReport/GetBelongToCompanyAssetReport",
-            data: { "YearMonth": $("#YearMonth").val() },
             datatype: "json",
             type: "post",
             success: function (result) {

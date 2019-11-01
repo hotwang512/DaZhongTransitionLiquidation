bbcce30754aa24d4be9e07f7da0492a132e9f057
landa <landa@DESKTOP-90C238M>
2019-11-01 14:29:46 +0800

    付款业务配置：订单业务配置维护供应商类别改为付款项目，配置供应商信息关联付款项目中配置的供应商

--- DaZhongTransitionLiquidation/Areas/CapitalCenterManagement/Model/Business_CustomerBankSetting.cs
+++ DaZhongTransitionLiquidation/Areas/CapitalCenterManagement/Model/Business_CustomerBankSetting.cs
@@ -8,6 +8,7 @@ namespace DaZhongTransitionLiquidation.Areas.CapitalCenterManagement.Model
     public class Business_CustomerBankSetting
     {
         public Guid VGUID { get; set; }
+        public Guid PurchaseItemVGUID { get; set; }
         public string CustomerID { get; set; }
         public string OrderVGUID { get; set; }
         public bool Isable { get; set; }

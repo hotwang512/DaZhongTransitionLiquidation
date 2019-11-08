f4610035c8c3d126e1632aab577c6a47bb3c983d
peter <peter@PETER-PC>
2019-11-08 11:03:42 +0800

    各模块添加默认首页

---
+++ DaZhongTransitionLiquidation/Areas/HomePage/Controllers/DefaultPageController.cs
@@ -0,0 +1,22 @@
+﻿using DaZhongTransitionLiquidation.Infrastructure.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DaZhongTransitionLiquidation.Areas.HomePage.Controllers
+{
+    public class DefaultPageController : BaseController
+    {
+        public DefaultPageController(DbService dbService, DbBusinessDataService dbBusinessDataService) : base(dbService, dbBusinessDataService)
+        {
+
+        }
+        // GET: HomePage/DefaultPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

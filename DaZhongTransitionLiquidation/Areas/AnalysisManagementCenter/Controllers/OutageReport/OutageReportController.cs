5e1c7b7d6e088ecae155d787fb72fda5419285c8
landa <landa@DESKTOP-90C238M>
2019-12-13 10:14:47 +0800

    停运车辆分析报表

---
+++ DaZhongTransitionLiquidation/Areas/AnalysisManagementCenter/Controllers/OutageReport/OutageReportController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DaZhongTransitionLiquidation.Infrastructure.ApiResultEntity;
+using DaZhongTransitionLiquidation.Infrastructure.Dao;
+using DaZhongTransitionLiquidation.Infrastructure.UserDefinedEntity;
+using SyntacticSugar;
+
+namespace DaZhongTransitionLiquidation.Areas.AnalysisManagementCenter.Controllers.OutageReport
+{
+    public class OutageReportController : BaseController
+    {
+        // GET: AnalysisManagementCenter/VehicleReport
+        public OutageReportController(DbService dbService, DbBusinessDataService dbBusinessDataService) : base(dbService, dbBusinessDataService)
+        {
+
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetManageCompanyOutageReport(string YearMonth, GridParams para)
+        {
+            
+            var currentYearDate = YearMonth.TryToDate().Year.ToString();
+            
+            var json = "";
+            var _db = DbBigDataConfig.GetInstance();
+            var sqlStr = @"select Period
+	                 ,abbr.Name as VehicleModel
+                     , Organization.Name         as CompanyName
+                     , Quantity                  = 1
+                from Cab.Cab_Work_Info_ByPeriod_FH        Cab_Work_Info
+                    left join Cab.Cab_Base_Info_D         Cab
+                        on Cab_Work_Info.CabId = Cab.CabId
+                    left join DZSrc.Organization_D        Organization
+                        on Cab.OrganizationID = Organization.OrganizationId
+                    left join DZSrc.VehicleAbbreviation_D abbr
+                        on abbr.VehicleAbbreviationId = Cab_Work_Info.VehicleAbbreviationId
+                where Period like '" + currentYearDate + @"%'
+                      and Cab_Work_Info.OrganizationId in ( 53, 54, 55, 198, 451 )
+                      and Cab_Work_Info.OperationStatus = 0";
+            var data = _db.Ado.GetDataTable(sqlStr);
+            json = data.DataTableToJson();
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ DaZhongTransitionLiquidation/Areas/AnalysisManagementCenter/Views/_js/OutageReport.js
@@ -0,0 +1,111 @@
+﻿//资产维护
+//所有元素选择器
+var selector = {
+    $grid: function () { return $("#jqxManageCompanyPeriodTable") },
+    $grid1: function () { return $("#jqxBelongToCompanyPeriodTable") },
+    $btnSearch: function () { return $("#btnSearch") },
+    $btnReset: function () { return $("#btnReset") },
+    $EditPermission: function () { return $("#EditPermission") }
+}; //selector end
+var isEdit = false;
+var vguid = "";
+function pickedFunc() {
+}
+var $page = function () {
+    this.init = function () {
+        var date = new Date;
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        month = (month < 10 ? "0" + month : month);
+        var currentDate = (year.toString() + "-" + month.toString());
+        $("#YearMonth").val(currentDate);
+        addEvent();
+        var winHeight = document.body.scrollWidth;
+        $("#assetReport").css("width", winHeight - 243);
+    }
+    //所有事件
+    function addEvent() {
+        //加载列表数据
+        initPeriodTable();
+        $("#btnGetData").on("click", function () {
+            initPeriodTable();
+        });
+        //关闭
+        $("#AssetReviewDialog_CancelBtn").on("click",
+            function () {
+                $("#AssetReviewDialog").modal("hide");
+            }
+        );
+    }; //addEvent end
+    function initPeriodTable() {
+        layer.load();
+        getPeriodData(function (datashow) {
+            var datafields = new Array();
+            datafields.push({ name: 'Period', type: 'string' });
+            datafields.push({ name: 'VehicleModel', type: 'string' });
+            datafields.push({ name: 'CompanyName', type: 'string' });
+            datafields.push({ name: 'Quantity', type: 'number' });
+            var source =
+            {
+                localdata: datashow,
+                datafields: datafields,
+                datatype: "json"
+            };
+            debugger;
+            var dataAdapter = new $.jqx.dataAdapter(source);
+            dataAdapter.dataBind();
+            var pivotDataSource = new $.jqx.pivot(dataAdapter,
+            {
+                pivotValuesOnRows: false,
+                totals: { rows: { subtotals: false, grandtotals: false }, columns: { subtotals: false, grandtotals: false } },
+                rows: [{ dataField: 'CompanyName' }, { dataField: 'VehicleModel' }],
+                columns: [{ dataField: 'Period', width: 110, align: 'center' }],
+                values: [
+                    {
+                        dataField: 'Quantity',
+                        'function': 'sum',
+                        text: '总数',
+                        formatSettings: { decimalPlaces: 0, align: 'center' },
+                        isHidden: true,
+                        sortable: false
+                    }
+                ]
+            });
+            selector.$grid().jqxPivotGrid(
+            {
+                source: pivotDataSource,
+                treeStyleRows: true,
+                autoResize: true,
+                selectionEnabled: true
+            });
+            var pivotGrid = selector.$grid().jqxPivotGrid('getInstance');
+            var pivotRows = pivotGrid.getPivotRows();
+            for (var k = 0; k < pivotRows.items.length; k++) {
+                pivotRows.items[k].expand();
+            }
+            var pivotCols = pivotGrid.getPivotColumns();
+            for (var i = 0; i < pivotCols.items.length; i++) {
+                pivotCols.items[i].expand();
+            }
+            pivotGrid.refresh();
+            layer.closeAll('loading');
+        });
+    }
+    var tableValue = "";
+    function getPeriodData(callback) {
+        $.ajax({
+            url: "/AnalysisManagementCenter/OutageReport/GetManageCompanyOutageReport",
+            data: { "YearMonth": $("#YearMonth").val() },
+            datatype: "json",
+            type: "post",
+            success: function (result) {
+                tableValue = result;
+                callback(result);
+            }
+        });
+    }
+};
+$(function () {
+    var page = new $page();
+    page.init();
+});

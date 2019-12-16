07b27c8fd4c9f5590b080fb188fb0bc8e074cbfb
landa <landa@DESKTOP-90C238M>
2019-12-16 17:21:47 +0800

    核定表修改

---
+++ DaZhongTransitionLiquidation/Areas/AnalysisManagementCenter/Controllers/FixedAssetsNetValue/FixedAssetsNetValueController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DaZhongTransitionLiquidation.Areas.AnalysisManagementCenter.Controllers.FixedAssetsNetValue
+{
+    public class FixedAssetsNetValueController : Controller
+    {
+        // GET: AnalysisManagementCenter/FixedAssetsNetValue
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

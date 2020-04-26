722ffd0fa3471c0ca8da5a4a9f6dea50fe3b82ec
peter <peter@PETER-PC>
2020-04-26 15:04:33 +0800

    1.结算车辆天数，平均天数SQL修改 2.资金对账算法修改，界面添加系统余额字段 3.资金对账表添加字段

--- DaZhongTransitionLiquidation/Areas/VoucherManageManagement/Controllers/VehicleUnit/VehicleUnitController.cs
+++ DaZhongTransitionLiquidation/Areas/VoucherManageManagement/Controllers/VehicleUnit/VehicleUnitController.cs
@@ -33,35 +33,39 @@ namespace DaZhongTransitionLiquidation.Areas.VoucherManageManagement.Controllers
                 {
                     yearMonth = searchParams.YearMonth.Replace("-", "");
                 }
-                response = db.SqlQueryable<Business_VehicleUnitList>(@" select g.* from (
-                             select a.VGUID,a.ORIGINALID,a.YearMonth,a.PLATE_NUMBER,
-                             m.BusinessName1 as MODEL_MAJOR, 
-                             m.BusinessName2 as MODEL_MINOR,
-                             --a.MODEL_MINOR,
-                             CAST(CAST(a.MODEL_DAYS AS decimal(18,2))/30 as decimal(18,0)) as MODEL_DAYS
-                            ,b.MANAGEMENT_COMPANY,
-                            b.BELONGTO_COMPANY,b.DESCRIPTION as CarType,b.GROUP_ID,b.OPERATING_STATE from Business_VehicleList as a 
-                            left join Business_AssetMaintenanceInfo as b on a.PLATE_NUMBER = b.PLATE_NUMBER 
-                            left join (select a.BusinessName as BusinessName1,b.BusinessName as BusinessName2,c.BusinessName as BusinessName3,c.VehicleAge from Business_ManageModel as a
-							left join Business_ManageModel as b on a.VGUID = b.ParentVGUID
-							left join Business_ManageModel as c on b.VGUID = c.ParentVGUID
-							where c.BusinessName is not null and c.VehicleAge is null
-							) as m on a.MODEL_MINOR = m.BusinessName3 
-							UNION ALL
-							    select a.VGUID,a.ORIGINALID,a.YearMonth,a.PLATE_NUMBER,
-                             m.BusinessName1 as MODEL_MAJOR, 
-                             m.BusinessName2 as MODEL_MINOR,
-                             CAST(CAST(a.MODEL_DAYS AS decimal(18,2))/30 as decimal(18,0)) as MODEL_DAYS
-                            ,b.MANAGEMENT_COMPANY,
-                            b.BELONGTO_COMPANY,b.DESCRIPTION as CarType,b.GROUP_ID,b.OPERATING_STATE from Business_VehicleList as a 
-                            left join Business_AssetMaintenanceInfo as b on a.PLATE_NUMBER = b.PLATE_NUMBER 
-                            left join (select a.BusinessName as BusinessName1,b.BusinessName as BusinessName2,c.BusinessName as BusinessName3,c.VehicleAge from Business_ManageModel as a
-							left join Business_ManageModel as b on a.VGUID = b.ParentVGUID
-							left join Business_ManageModel as c on b.VGUID = c.ParentVGUID
-							where c.BusinessName is not null and c.VehicleAge is not null
-							) as m on a.MODEL_MINOR = m.BusinessName3  and b.VEHICLE_AGE = m.VehicleAge) as g
-                            where    g.GROUP_ID='出租车' and g.OPERATING_STATE='在运' and g.MODEL_MAJOR is not null ")
-                .WhereIF(searchParams.YearMonth != null, i => i.YearMonth == yearMonth)
+                response = db.SqlQueryable<Business_VehicleUnitList>(@"select a.VGUID,a.ORIGINALID,a.YearMonth,a.PLATE_NUMBER,b.MODEL_MINOR,CAST(CAST(a.MODEL_DAYS AS decimal(18,2))/30 as decimal(18,0)) as MODEL_DAYS
+                            , b.MANAGEMENT_COMPANY, b.BELONGTO_COMPANY,b.MODEL_MAJOR, b.DESCRIPTION as CarType from Business_VehicleList as a left join Business_AssetMaintenanceInfo
+                            as b on a.PLATE_NUMBER = b.PLATE_NUMBER where b.OPERATING_STATE='在运' and b.GROUP_ID='出租车'  and b.MODEL_MAJOR is not null ")
+
+               //         response = db.SqlQueryable<Business_VehicleUnitList>(@" select g.* from (
+               //                      select a.VGUID,a.ORIGINALID,a.YearMonth,a.PLATE_NUMBER,
+               //                      m.BusinessName1 as MODEL_MAJOR, 
+               //                      m.BusinessName2 as MODEL_MINOR,
+               //                      --a.MODEL_MINOR,
+               //                      CAST(CAST(a.MODEL_DAYS AS decimal(18,2))/30 as decimal(18,0)) as MODEL_DAYS
+               //                     ,b.MANAGEMENT_COMPANY,
+               //                     b.BELONGTO_COMPANY,b.DESCRIPTION as CarType,b.GROUP_ID,b.OPERATING_STATE from Business_VehicleList as a 
+               //                     left join Business_AssetMaintenanceInfo as b on a.PLATE_NUMBER = b.PLATE_NUMBER 
+               //                     left join (select a.BusinessName as BusinessName1,b.BusinessName as BusinessName2,c.BusinessName as BusinessName3,c.VehicleAge from Business_ManageModel as a
+               //left join Business_ManageModel as b on a.VGUID = b.ParentVGUID
+               //left join Business_ManageModel as c on b.VGUID = c.ParentVGUID
+               //where c.BusinessName is not null and c.VehicleAge is null
+               //) as m on a.MODEL_MINOR = m.BusinessName3 
+               //UNION ALL
+               //    select a.VGUID,a.ORIGINALID,a.YearMonth,a.PLATE_NUMBER,
+               //                      m.BusinessName1 as MODEL_MAJOR, 
+               //                      m.BusinessName2 as MODEL_MINOR,
+               //                      CAST(CAST(a.MODEL_DAYS AS decimal(18,2))/30 as decimal(18,0)) as MODEL_DAYS
+               //                     ,b.MANAGEMENT_COMPANY,
+               //                     b.BELONGTO_COMPANY,b.DESCRIPTION as CarType,b.GROUP_ID,b.OPERATING_STATE from Business_VehicleList as a 
+               //                     left join Business_AssetMaintenanceInfo as b on a.PLATE_NUMBER = b.PLATE_NUMBER 
+               //                     left join (select a.BusinessName as BusinessName1,b.BusinessName as BusinessName2,c.BusinessName as BusinessName3,c.VehicleAge from Business_ManageModel as a
+               //left join Business_ManageModel as b on a.VGUID = b.ParentVGUID
+               //left join Business_ManageModel as c on b.VGUID = c.ParentVGUID
+               //where c.BusinessName is not null and c.VehicleAge is not null
+               //) as m on a.MODEL_MINOR = m.BusinessName3  and b.VEHICLE_AGE = m.VehicleAge) as g
+               //                     where    g.GROUP_ID='出租车' and g.OPERATING_STATE='在运' and g.MODEL_MAJOR is not null ")
+               .WhereIF(searchParams.YearMonth != null, i => i.YearMonth == yearMonth)
                 .WhereIF(searchParams.PLATE_NUMBER != null, i => i.PLATE_NUMBER.Contains(searchParams.PLATE_NUMBER))
                 .WhereIF(searchParams.MODEL_MINOR != null, i => i.MODEL_MINOR.Contains(searchParams.MODEL_MINOR))
                 //.WhereIF(searchParams.MODEL_DAYS != null, i => i.MODEL_DAYS == searchParams.MODEL_DAYS)
